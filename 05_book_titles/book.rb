ce2895dc1c7cd33b49da2b2307ed4cbf97712a14
Josh Cooley <jcooley10@gmail.com>
2019-08-23 20:31:32 -0400

    Finished book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(newtitle)
+    words = newtitle.split(" ")
+    words[0] = words[0].upcase
+    words = words.map do |word|
+      if "and in the of".include?(word) && word !="i"
+        word
+      else
+        word.capitalize
+      end
+    end
+    @title = words.join(" ")
+  end
 end
